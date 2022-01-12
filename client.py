ff1f287be8dad2278e8f1d189d4e4b8dfeeccadd
Piotr Szachewicz <p.szachewicz@stud.elka.pw.edu.pl>
2022-01-12 15:58:22 +0100

    Add predictions to server

--- client.py
+++ client.py
@@ -2,12 +2,45 @@
 
 from flask import Flask, request, json
 import requests
+from argparse import ArgumentParser
 
 app = Flask(__name__)
 
 
+def parse_arguments():
+    p = ArgumentParser("Client for sending requests to prediction server")
+    p.add_argument('-c',
+                   '--city',
+                   help="Target city for delivery.",
+                   type=str,
+                   default="Warszawa",
+                   choices=[
+                       "Warszawa", "Wrocław", "Poznań", "Szczecin", "Kraków",
+                       "Radom", "Gdynia"
+                   ])
+    p.add_argument('-d',
+                   '--delivery-company',
+                   help="Delivery company.",
+                   type=int,
+                   default=620,
+                   choices=[254, 360, 516, 620])
+    p.add_argument('-t',
+                   '--time',
+                   help="Date of purchase.",
+                   type=str,
+                   default="2021-03-13T15:20:42")
+    return p.parse_args()
+
+
 def main():
-    data = {"name": "df"}
+    args = parse_arguments()
+    data = {
+        "purchase_timestamp": "2021-03-13T15:20:42",
+        "city": ["Warszawa"],
+        "delivery_company": [620]
+    }
+
+
     print("Sending request:\n", data)
     response = requests.get("http://127.0.0.1:5000/", json=data)
     print("Got response:")
--- server.py
+++ server.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, json
+from sklearn.linear_model import LogisticRegression
+import joblib
+import pandas as pd
 
+MLP_classifier = joblib.load("MLP_model2.sav")
 app = Flask(__name__)
 
 
 def process_data(data: dict) -> dict:
-    name = request.json["name"]
+    name = request.json["city"]
     response_data = {"name": name, "url": "www"}
-    return response_data
+    ls=['monday', 'tuesday','wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    flag_morning = 0
+    data_df = pd.DataFrame(data)
+    data_df['purchase_timestamp'] = data_df['purchase_timestamp'].apply(lambda x: pd.to_datetime(x))
+    data_df['delivery_weekday'] = data_df['purchase_timestamp'].apply(lambda x: ls[x.weekday()])
+    data_df['purchase_morning'] = data_df['purchase_timestamp'].apply(lambda x: 1 if x.hour<=14 else 0)
+    if data_df['purchase_morning'][0] == 1:
+        flag_morning = 1
+    data_df.drop(["purchase_timestamp","purchase_morning"], axis=1, inplace=True)
+
+    final_df = {} 
+    column_names = ['purchase_morning', 'Kraków','Poznań','Radom', 'Szczecin', 'Warszawa', 'Wrocław',	360, 516, 620, 'monday', 'saturday', 'sunday', 'thursday', 'tuesday', 'wednesday']
+    for feature in column_names:
+        final_df.setdefault(feature,[0])
+    features = pd.DataFrame(final_df)
+
+    for inp_el in data_df.columns:
+        if data_df[inp_el][0] in features.columns:
+            features[data_df[inp_el][0]][0]=1
+    if flag_morning==1:
+        features["purchase_morning"][0]=1
+    
+    features = features.rename(columns={360:'delivery_360', 254:'delivery_254', 516:'delivery_516', 620:'delivery_620'})
+    pred = MLP_classifier.predict(features)
+    print(f"predicted delivery days: {pred}")
+
+    return data_df.to_dict()
 
 
 @app.route('/', methods=["GET"])
