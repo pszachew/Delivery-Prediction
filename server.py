374f86b4761ea071d9f61f937e9b0450e94e6d2f
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-01-14 13:19:45 +0100

    Format server code

--- server.py
+++ server.py
@@ -13,40 +13,58 @@ app = Flask(__name__)
 def get_prediction(data: dict) -> dict:
     name = request.json["city"]
     response_data = {"name": name, "url": "www"}
-    ls=['monday', 'tuesday','wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    ls = [
+        'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday',
+        'sunday'
+    ]
     flag_morning = 0
     data['city'] = [data['city']]
-        
+
     data_df = pd.DataFrame(data)
-    data_df['purchase_timestamp'] = data_df['purchase_timestamp'].apply(lambda x: pd.to_datetime(x))
-    data_df['delivery_weekday'] = data_df['purchase_timestamp'].apply(lambda x: ls[x.weekday()])
-    data_df['purchase_morning'] = data_df['purchase_timestamp'].apply(lambda x: 1 if x.hour<=14 else 0)
+    data_df['purchase_timestamp'] = data_df['purchase_timestamp'].apply(
+        lambda x: pd.to_datetime(x))
+    data_df['delivery_weekday'] = data_df['purchase_timestamp'].apply(
+        lambda x: ls[x.weekday()])
+    data_df['purchase_morning'] = data_df['purchase_timestamp'].apply(
+        lambda x: 1 if x.hour <= 14 else 0)
     if data_df['purchase_morning'][0] == 1:
         flag_morning = 1
-    data_df.drop(["purchase_timestamp","purchase_morning"], axis=1, inplace=True)
+    data_df.drop(["purchase_timestamp", "purchase_morning"],
+                 axis=1,
+                 inplace=True)
 
-    final_df = {} 
-    column_names = ['purchase_morning', 'Kraków','Poznań','Radom', 'Szczecin', 'Warszawa', 'Wrocław',	360, 516, 620, 'monday', 'saturday', 'sunday', 'thursday', 'tuesday', 'wednesday']
+    final_df = {}
+    column_names = [
+        'purchase_morning', 'Kraków', 'Poznań', 'Radom', 'Szczecin',
+        'Warszawa', 'Wrocław', 360, 516, 620, 'monday', 'saturday', 'sunday',
+        'thursday', 'tuesday', 'wednesday'
+    ]
     for feature in column_names:
-        final_df.setdefault(feature,[0])
+        final_df.setdefault(feature, [0])
     features = pd.DataFrame(final_df)
 
     for inp_el in data_df.columns:
         if data_df[inp_el][0] in features.columns:
-            features[data_df[inp_el][0]][0]=1
-    if flag_morning==1:
-        features["purchase_morning"][0]=1
-    
-    features = features.rename(columns={360:'delivery_360', 254:'delivery_254', 516:'delivery_516', 620:'delivery_620'})
+            features[data_df[inp_el][0]][0] = 1
+    if flag_morning == 1:
+        features["purchase_morning"][0] = 1
+
+    features = features.rename(
+        columns={
+            360: 'delivery_360',
+            254: 'delivery_254',
+            516: 'delivery_516',
+            620: 'delivery_620'
+        })
     pred = None
-    if data["model"]=="MLP":
-        pred=MLP_classifier.predict(features)
+    if data["model"] == "MLP":
+        pred = MLP_classifier.predict(features)
     elif data["model"] == "REG":
-        pred=Regression_classifier.predict(features)
+        pred = Regression_classifier.predict(features)
     else:
         raise ValueError("Wrong value for model type")
     print(f"predicted delivery days: {pred}")
-    out_dict = {"days":int(pred[0])}
+    out_dict = {"days": int(pred[0])}
     return out_dict
 
 
